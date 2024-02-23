0b02352a0cd5a56c89d722cd2601c849c11e173b
Philippe-Morin-Veer <157501225+Philippe-Morin-Veer@users.noreply.github.com>
2024-02-23 08:55:34 -0500

    exec pour provenance

---
+++ wfa_hobbyDepart/wfa_hobby/ManagerProvenance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wfa_hobby
+{
+    internal class ManagerProvenance
+    {
+        public List<Provenance> ListerProvenance()
+        {
+            List<Provenance> maListe = new List<Provenance>();
+            try
+            {
+                using (var maConnection = new SqlConnection(Properties.Settings.Default.maConnectionString))
+                {
+                    using (var maCommande = new SqlCommand("listerProvenances", maConnection))
+                    {
+                        maCommande.CommandType = System.Data.CommandType.StoredProcedure;
+                        maCommande.Connection = maConnection;
+                        using (var monDataReader = maCommande.ExecuteReader())
+                        {
+                            while (monDataReader.Read())
+                            {
+                                Provenance provenance = new Provenance();
+                                provenance.No_provenance = (int)monDataReader["no_provenance"];
+                                provenance.LaProvenance = monDataReader["Provenance"].ToString();
+                                maListe.Add(provenance);
+                            }
+                        }
+
+                    }
+                    return maListe;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ wfa_hobbyDepart/wfa_hobby/Provenance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wfa_hobby
+{
+    internal class Provenance
+    {
+        private int no_provenance;
+        private string laProvenance;
+
+        public int No_provenance { get => no_provenance; set => no_provenance = value; }
+        public string LaProvenance { get => laProvenance; set => laProvenance = value; }
+    }
+}
